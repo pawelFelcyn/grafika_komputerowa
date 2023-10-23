6fdfa87dfba8d706e656439a9ed4f828a906c507
Paweł Felcyn <pawfel1@st.amu.edu.pl>
2023-10-23 16:54:10 +0200

    2_1a

--- cw 2/src/ex_2_1a.hpp
+++ cw 2/src/ex_2_1a.hpp
@@ -11,9 +11,10 @@
 
 #include "Box.cpp"
 
-
+float degreesToRadians(float degrees);
 GLuint program;
 Core::Shader_Loader shaderLoader;
+float yRotation;
 
 GLuint VAO,VBO;
 void renderScene(GLFWwindow* window)
@@ -28,18 +29,25 @@ void renderScene(GLFWwindow* window)
 	// W funkcji init należy przesłać pozycje i kolory do GPU 
 
 	glUseProgram(program);
-
-
-	glm::mat4 transformation;
+	yRotation = time * 30;
+	glm::mat4 transformation = glm::translate(glm::vec3(0.5f, 0.5f, -0.2f))
+		* glm::eulerAngleXYZ(degreesToRadians(45.0f), degreesToRadians(yRotation), degreesToRadians(25.0f));
 	glUniformMatrix4fv(glGetUniformLocation(program, "transformation"), 1, GL_FALSE, (float*)&transformation);
 
 
 	// Powiąż stworzone VAO za pomocą funkcji glBindVertexArray
 	// wykorzystaj glDrawArrays do narysowania prostopadłościanu
+	glBindVertexArray(VAO);
+	glDrawArrays(GL_TRIANGLES, 0, 36);
 
 	glUseProgram(0);
 	glfwSwapBuffers(window);
 }
+
+float degreesToRadians(float degrees) {
+	return degrees * (3.14159265358f / 180);
+}
+
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
 	glViewport(0, 0, width, height);
@@ -56,18 +64,25 @@ void init(GLFWwindow* window)
 	// Zainicjalizuj VertexArrayObject (VAO) i zapisz w nim atrybuty prostopadłościanu
 	// I Stwórz VAO
 	// 1. Stwórz nowy VAO za pomocą glGenVertexArrays i przypisz jego adres do zmiennej globalnej
+	glGenVertexArrays(1, &VAO);
 	// 2. Powiąż stworzone VAO za pomocą funkcji glBindVertexArray
+	glBindVertexArray(VAO);
 	// II Zainicjalizuj VBO
 	// 3. Stwórz VertexBufferObject do przechowywania pozycji wierzchołków za pomocą funkcji glGenBuffers
+	glGenBuffers(1, &VBO);
 	// 4. Powiąż stworzone VBO za pomocą funkcji glBindBuffer(GLenum target, GLuint )
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	// 5. Zaalokuj w GPU miejsce na tablice box i zaalakokuj ją za pomocą glBufferData
-	//std::cout << sizeof(box);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(box), &box, GL_STATIC_DRAW);
 	// II Stwórz definicję danych w buforze
-	// 6. Aktywuj atrybut powiązany z pozycją wierchołków za pomocą glEnableVertexAttribArray(GLuint index). Indeks jest zapisany w shaderze wierzchołków w 3. lini. Można odpytać shader o indeks za pomocą funkcji glGetAttribLocation(GL uint program, const GLchar *name)
+	// 6. Aktywuj atrybut powiązany z pozycją wierchołków za pomocą glEnableVertexAttribArray(GLuint index). 
+	//    Indeks jest zapisany w shaderze wierzchołków w 3. lini. Można odpytać shader o indeks za pomocą funkcji glGetAttribLocation(GL uint program, const GLchar *name)
+	glEnableVertexAttribArray(0);
 	// 6. Aktywuj atrybut powiązany z kolorem wierchołków za pomocą glEnableVertexAttribArray(GLuint index). Indeks jest zapisany w shaderze wierzchołków w 4. lini. Można odpytać shader o indeks za pomocą funkcji glGetAttribLocation(GL uint program, const GLchar *name)
-
+	glEnableVertexAttribArray(1);
 	// 8. Zdefiniuj jak OpenGL powinien interpretować dane za pomocą glVertexAttribPointer
-
+	glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, sizeof(float) * 8, 0);
+	glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, sizeof(float) * 8, (void*)(sizeof(float) * 4));
 	// w renderScene!
 	//glBindVertexArray(VAO);
 	///glDrawArrays(GL_TRIANGLES, 0, 36);
